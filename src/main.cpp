f211ad039c29cf09bcc2f9374ab469ec6994839f
Artem <adz@it-rkomi.ru>
2021-12-03 03:36:18 +0300

    test function

--- src/main.cpp
+++ src/main.cpp
@@ -140,10 +140,17 @@ void initPins(){
     pinMode (ENA_PIN, OUTPUT);
     pinMode (IN1_PIN, OUTPUT);
     pinMode (IN2_PIN, OUTPUT);
-   /* pinMode (2, OUTPUT);
+    pinMode (11, OUTPUT);
+    pinMode (10, OUTPUT);
+    pinMode (9, OUTPUT);
+    pinMode (8, OUTPUT);
+    pinMode (7, OUTPUT);
+    pinMode (6, OUTPUT);
+    pinMode (5, OUTPUT);
     pinMode (4, OUTPUT);
+    pinMode (2, OUTPUT);
     pinMode (3, OUTPUT);
-    pinMode (1, OUTPUT);*/
+    pinMode (1, OUTPUT);
 }
 void onStartWifi(){}
 
@@ -219,6 +226,34 @@ void startWebSocket() {
     webSocket.onEvent( webSocketEvent);
     Serial.println("WebSocket server started.");
 }
+void testOn() {
+    digitalWrite (11, HIGH);
+    digitalWrite (10, HIGH);
+    digitalWrite (9, HIGH);
+    digitalWrite (8, HIGH);
+    digitalWrite (7, HIGH);
+    digitalWrite (6, HIGH);
+    digitalWrite (5, HIGH);
+    digitalWrite (4, HIGH);
+    digitalWrite (3, HIGH);
+    digitalWrite (2, HIGH);
+    digitalWrite (1, HIGH);
+    delay(2000);
+}
+void testOf() {
+    digitalWrite (11, LOW);
+    digitalWrite (10, LOW);
+    digitalWrite (9, LOW);
+    digitalWrite (8, LOW);
+    digitalWrite (7, LOW);
+    digitalWrite (6, LOW);
+    digitalWrite (5, LOW);
+    digitalWrite (4, LOW);
+    digitalWrite (3, LOW);
+    digitalWrite (2, LOW);
+    digitalWrite (1, LOW);
+    delay(2000);
+}
 
 void setup() {
     delay(1000);
@@ -233,13 +268,19 @@ void setup() {
     onStartWifi();
     startWebSocket();
 }
-
+bool test = true;
 void loop() {
+    if(test){
+        testOn();
+        test = false;
+    }else{
+        testOf();
+        test = true;
+    }
     webSocket.loop();
     lightingLoop(pLedModel);
     drivingLoop(pDriveModel);
-
-    //digitalWrite (2, 1);
+    //digitalWrite (4, 1);
     //digitalWrite (4, 1);
     //digitalWrite (3, 1);
     //digitalWrite (1, 1);
